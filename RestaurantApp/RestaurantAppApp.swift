570a1e8ed967e51651ec4e59d70fcf3720726c5f
HarivanshL <112008912+HarivanshL@users.noreply.github.com>
2023-10-07 17:08:21 -0500

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Harivansh Luchmun on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
